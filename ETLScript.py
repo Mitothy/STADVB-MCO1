357f5d95a29b90c5eec5edddf7b038b26cc78328
Timothy Tan <98691592+Mitothy@users.noreply.github.com>
2024-02-15 00:21:54 +0800

    Update ETLSCript
    added import for SQLAlchemy and Pandas
    
    Replaced Null Values in px.csv with 0
    Removed Unnecessary line breaks in doctors.csv

--- ETLScript.py
+++ ETLScript.py
@@ -1,8 +1,9 @@
-import MySQLdb # TO IMPORT: python -m  pip install mysqlclient
+import MySQLdb # TO IMPORT: pip install mysqlclient
 import pandas as pd #TO IMPORT: pip install pandas
+import sqlalchemy as sa # TO IMPORT: python pip install sqlalchemy
 
 # Change "YOURUSER" to Local User and "YOURPASSWORD" to Local Password
-connection = MySQLdb.connect("localhost",  "Tim", "Tif2003#", "seriousmd") 
+connection = MySQLdb.connect("localhost",  "YOURUSER", "YOURPASSWORD", "seriousmd") 
 
 # Create cursor and use it to execute SQL command
 cursor = connection.cursor()
@@ -13,5 +14,21 @@ clinicsdf = pd.read_csv('clinics.csv', encoding="ISO-8859-1")
 pxdf = pd.read_csv('px.csv', encoding="ISO-8859-1")
 appointmentsdf = pd.read_csv('appointments.csv', encoding="ISO-8859-1")
 
+"""
+CLEANING
+"""
+# Replace Null Values with 0 in px.csv
+pxdf['age'] = pxdf['age'].fillna(0)
+
+# Remove unnecessary line breaks in doctors.csv
+doctorsdf = doctorsdf.replace('\n','', regex=True)
+
+# Showing it worked
+print(doctorsdf.iloc[10590:10600].to_string(index=False))
+
+"""
+LOADING TO MYSQL
+"""
+
 # Close Connection to mySQL
-connection.close()
+connection.close()  
