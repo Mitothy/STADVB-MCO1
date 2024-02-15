3d62c186d11f0a7c1f720973ed756b28dbe97bd2
Timothy Tan <98691592+Mitothy@users.noreply.github.com>
2024-02-15 16:02:28 +0800

    More Cleaning

--- ETLScript.py
+++ ETLScript.py
@@ -17,15 +17,15 @@ appointmentsdf = pd.read_csv('appointments.csv', encoding="ISO-8859-1")
 """
 CLEANING
 """
-
 # Cleaning px.csv
 pxdf['age'] = pxdf['age'].fillna(0)
 pxdf['age'] = pd.to_numeric(pxdf['age'], errors='coerce')
 pxdf = pxdf.drop_duplicates(subset='pxid', keep='first')
 pxdf.loc[pxdf['age'] > 116, 'age'] = -1 # Oldest recorded age is 116
 
-
 # Cleaning doctor.csv
+doctorsdf['age'] = pd.to_numeric(doctorsdf['age'], errors='coerce').fillna(-1)  
+doctorsdf['mainspecialty'].fillna('Unknown', inplace=True)
 doctorsdf = doctorsdf.replace('\n','', regex=True)
 doctorsdf = doctorsdf.drop_duplicates(subset='doctorid', keep='first')
 
@@ -36,6 +36,12 @@ clinicsdf = clinicsdf.drop_duplicates(subset='clinicid', keep='first')
 appointmentsdf = appointmentsdf[appointmentsdf['pxid'].isin(pxdf['pxid'])]
 appointmentsdf = appointmentsdf[appointmentsdf['doctorid'].isin(doctorsdf['doctorid'])]
 appointmentsdf = appointmentsdf[appointmentsdf['clinicid'].isin(clinicsdf['clinicid'])]
+appointmentsdf['TimeQueued'] = pd.to_datetime(appointmentsdf['TimeQueued'], format='%Y-%m-%d %H:%M:%S.%f', errors='coerce')
+appointmentsdf['QueueDate'] = pd.to_datetime(appointmentsdf['QueueDate'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
+appointmentsdf['StartTime'] = pd.to_datetime(appointmentsdf['StartTime'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
+appointmentsdf['EndTime'] = pd.to_datetime(appointmentsdf['EndTime'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
+
+print(appointmentsdf)
 
 """
 LOADING TO MYSQL
