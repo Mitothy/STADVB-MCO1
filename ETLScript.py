3b30be9e07d6b8766c4428bc4942a44a3f4752ba
Timothy Tan <98691592+Mitothy@users.noreply.github.com>
2024-02-15 19:12:35 +0800

    DONT TRY THIS ON UR DEVICE FOR NOW

--- ETLScript.py
+++ ETLScript.py
@@ -18,7 +18,6 @@ connection = MySQLdb.connect("localhost",  "Tim", "Tif2003#", "seriousmd")
 # Create engine for loading into mySQL
 engine = sa.create_engine("mysql+mysqldb://Tim:Tif2003#@localhost/seriousmd")
 
-
 # Create cursor and use it to execute SQL command
 cursor = connection.cursor()
 
@@ -28,16 +27,16 @@ clinicsdf = pd.read_csv('clinics.csv', encoding="ISO-8859-1")
 pxdf = pd.read_csv('px.csv', encoding="ISO-8859-1")
 appointmentsdf = pd.read_csv('appointments.csv', encoding="ISO-8859-1")
 
+print(len(pxdf))
+
 """
 CLEANING
 """
 # Cleaning px.csv
-pxdf['age'] = pxdf['age'].fillna(0)
-pxdf['age'] = pd.to_numeric(pxdf['age'], errors='coerce')
 pxdf = pxdf.drop_duplicates(subset='pxid', keep='first')
 pxdf['gender'] = pxdf['gender'].apply(clean_gender)
-pxdf.loc[pxdf['age'] > 116, 'age'] = 999 # Oldest recorded age is 116
-pxdf.loc[pxdf['age'] < 0, 'age'] = 999 # Oldest recorded age is 116
+pxdf = pxdf.dropna(subset=['age'])
+pxdf = pxdf[(pxdf['age'] >= 0) & (pxdf['age'] <= 116)] # Oldest Recorded Age
 
 # Cleaning doctor.csv
 doctorsdf['age'] = pd.to_numeric(doctorsdf['age'], errors='coerce').fillna(999)  
@@ -57,18 +56,16 @@ appointmentsdf['QueueDate'] = pd.to_datetime(appointmentsdf['QueueDate'], format
 appointmentsdf['StartTime'] = pd.to_datetime(appointmentsdf['StartTime'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
 appointmentsdf['EndTime'] = pd.to_datetime(appointmentsdf['EndTime'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
 
-#print(pxdf.iloc[548:552])
-
-
 """
 LOADING TO MYSQL
 
-doctorsdf.to_sql('dim_doc', con=engine, if_exists='append', index=False)
+clinicsdf.to_sql('dim_clinic', con=engine, if_exists='append', index=False)
 pxdf.to_sql('dim_px', con=engine, if_exists='append', index=False)
+doctorsdf.to_sql('dim_doc', con=engine, if_exists='append', index=False)
 appointmentsdf.to_sql('fact_appt', con=engine, if_exists='append', index=False)
 """
 
-clinicsdf.to_sql('dim_clinic', con=engine, if_exists='append', index=False)
+print(len(pxdf))
 
 # Close Connection to mySQL
 cursor.close()
